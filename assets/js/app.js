5830ee643021b69b4a0b7b11db362561392f16b8
ylee1101 <yongho19911101@hotmail.com>
2017-07-25 18:16:21 -0700

    added more

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,12 +6,14 @@ var APIKey = "3e43d0053a0243f29982948ebc1ab07e";
 var queryURL = "https://api.giphy.com/v1/gifs/search?";
 var state = "animated";
 
-//for soccer, boxing and baseball variables
+//for soccer, boxing and etc variables
 // var topics = [];
 
 function search() {
+
   gifSearchBtn = $("#gifSearchBtn");
   var searchButton = $("#search").val();
+  console.log(searchButton);
   searchBtn = $("<button>");
   searchBtn.attr("data-value", searchButton);
   searchBtn.text(searchButton);
@@ -20,8 +22,8 @@ function search() {
 
 };
 
-  $("button").on("click", function(){
-    var sports = $(this).attr("data-value");
+function downloadgifs () {
+  var sports = $(this).attr("data-value");
     console.log(sports);
     gifsGoHere.empty()
     var ajaxURL = queryURL+"q="+sports+"&api_key="+APIKey;
@@ -31,8 +33,10 @@ function search() {
       url: ajaxURL, 
       method: "GET"
     }).done(function(response) {
+      console.log(response);
       for(var i = 0; i < response.data.length; i++){
         var searchDiv = $("<div>");
+        searchDiv.addClass("inline");
         var p = $("<p>").text("Rating: " + response.data[i].rating);
         var searchGif = $("<img>");
 
@@ -43,13 +47,30 @@ function search() {
         searchGif.addClass("giph");
 
         searchGif.attr("src", response.data[i].images.fixed_height.url);
-        searchGif.append(p);
-        searchGif.append(response.data);
-        $("gifsGoHere").append(resultGif);
+        searchDiv.append(p);
+        searchDiv.append(searchGif);
+        $("#gifsGoHere").append(searchDiv);
       }
     });
-  });
+};
+
+function animateGif (){
+  var state = $(this).attr("state");
+  console.log(state);
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("animate-url"));
+        $(this).attr("state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("still-url"));
+        $(this).attr("state", "still");
+      }
+};
 
 
+$(document).ready(function(){
+  $(document).on("click", "#searchButton", search);
+  $(document).on("click", ".gifbutton", downloadgifs);
+  $(document).on("click", ".giph", animateGif);
+});
 
-
+//need to resize the still image with animated image & have spacing with individual gifs
